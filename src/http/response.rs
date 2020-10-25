cd6f9614b8ebedd78b34edff3fe54c829dd17dd1
Rodolfo De Nadai <rdenadai@gmail.com>
2020-10-24 23:16:58 -0300

    better logic

--- src/http/response.rs
+++ src/http/response.rs
@@ -13,7 +13,11 @@ impl Response {
         Response { status_code, body }
     }
 
-    pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+    // pub fn send(&self, stream: &mut TcpStream) -> IoResult<()> {
+    // Dynamic dispatch implemented with dyn keyword... runtime cost
+    // pub fn send(&self, stream: &mut dyn Write) -> IoResult<()> {
+    // Static dispatch implemented with impl keyword... bigger binary, compilation cost
+    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
         let body = match &self.body {
             Some(b) => b,
             None => "",
